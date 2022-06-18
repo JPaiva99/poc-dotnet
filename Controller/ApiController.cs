a30b3f0e8b58316f45998c394693195c595c2cb1
Filipe Castro <filipedecastroresende@outlook.com>
2022-06-18 13:52:08 -0300

    [feature] add swagger and controller

---
+++ Controller/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+        [HttpGet("gerarNumero")]
+        public async Task<ActionResult<int>> GenerateNumber([FromQuery]int min, int max)
+        {
+            var randomGenerator = new Random();
+            return Ok(randomGenerator.Next(min, max));
+        }
+    }
+}
+
